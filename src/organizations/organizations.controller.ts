ef12b567e991e56ee43f3d49d1fbf7ff429dcb37
swallowtail62 <swallowtail.a7@gmail.com>
2023-04-13 11:52:31 +0900

    feat: Install & configure @nestjs/swagger

--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -2,7 +2,9 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { Organization } from './domain';
 import { CreateOrganizationDto } from './dto/create-organization.dto';
+import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('organizations')
 @Controller({
   path: 'organizations',
 })
@@ -15,6 +17,14 @@ export class OrganizationsController {
   }
 
   @Post()
+  @ApiBody({
+    description: 'Create an organization',
+    type: CreateOrganizationDto,
+  })
+  @ApiCreatedResponse({
+    description: 'The record has been successfully created.',
+    type: Organization,
+  })
   create(@Body() { name }: CreateOrganizationDto) {
     return this.organizationsService.create({ name });
   }
