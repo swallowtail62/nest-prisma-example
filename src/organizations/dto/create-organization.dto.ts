c14bd83eca26562eb28502c0287ba9467200ab09
swallowtail62 <swallowtail.a7@gmail.com>
2023-04-17 17:15:37 +0900

    feat: Define openapi with `zod-nestjs`

--- src/organizations/dto/create-organization.dto.ts
+++ src/organizations/dto/create-organization.dto.ts
@@ -1,3 +1,22 @@
-export class CreateOrganizationDto {
-  name: string;
-}
+import { createZodDto } from '@anatine/zod-nestjs';
+import { extendApi } from '@anatine/zod-openapi';
+import { z } from 'zod';
+
+const organizationZ = extendApi(
+  z.object({
+    id: z.string(),
+    name: z.string().min(1).max(30),
+  }),
+);
+
+export class GetOrganizationResponse extends createZodDto(organizationZ) {}
+
+export class CreateOrganizationDto extends createZodDto(
+  organizationZ.omit({ id: true }),
+) {}
+
+export class ListOrganizationsDto extends createZodDto(
+  z.object({
+    organizations: z.array(organizationZ),
+  }),
+) {}
--- src/organizations/organizations.controller.ts
+++ src/organizations/organizations.controller.ts
@@ -1,19 +1,50 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, UsePipes } from '@nestjs/common';
 import { OrganizationsService } from './organizations.service';
 import { Organization } from './domain';
-import { CreateOrganizationDto } from './dto/create-organization.dto';
-import { ApiBody, ApiCreatedResponse, ApiTags } from '@nestjs/swagger';
+import {
+  CreateOrganizationDto,
+  GetOrganizationResponse,
+  ListOrganizationsDto,
+} from './dto/create-organization.dto';
+import {
+  ApiBody,
+  ApiCreatedResponse,
+  ApiOkResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { ZodValidationPipe } from '@anatine/zod-nestjs';
 
 @ApiTags('organizations')
 @Controller({
   path: 'organizations',
 })
+@UsePipes(ZodValidationPipe)
 export class OrganizationsController {
   constructor(private readonly organizationsService: OrganizationsService) {}
 
   @Get()
-  findAll(): Promise<Organization[]> {
-    return this.organizationsService.findAll();
+  @ApiOkResponse({
+    description: 'List of organizations',
+    type: GetOrganizationResponse,
+    isArray: true,
+  })
+  async findAll() {
+    // return this.organizationsService.findAll();
+    // 極論、何も返してないのに open api schema 的には返り値があったりする可能性はあるし、
+    // DTO class と array response の相性が悪い...
+    // async findAll(): Promise<GetOrganizationResponse[]>
+    // として運用できないことはないが、 class が使いづらい...
+
+    /**
+    organizations.map((o) => {
+      const dto = new GetOrganizationResponse();
+      dto.id = o.id;
+      dto.name = o.name;
+      return dto;
+    });
+     */
+    // って感じにやらなきゃいけない...
+    const organizations = await this.organizationsService.findAll();
   }
 
   @Post()
@@ -23,7 +54,7 @@ export class OrganizationsController {
   })
   @ApiCreatedResponse({
     description: 'The record has been successfully created.',
-    type: Organization,
+    type: ListOrganizationsDto,
   })
   create(@Body() { name }: CreateOrganizationDto) {
     return this.organizationsService.create({ name });
